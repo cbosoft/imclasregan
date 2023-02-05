538c874fcdd902731ff1798ad782ed0f754057cf
Christopher Boyle <chrismjboyle@gmail.com>
2023-02-05 14:18:52 +0000

    Measure time taken; thank user for their efforts

--- site/index.js
+++ site/index.js
@@ -2,6 +2,7 @@
 var current_image = null;
 var progress = 0;
 var session_id = '';
+var time_start = null;
 
 function uuidv4() {
     return ([1e7] + -1e3 + -4e3 + -8e3 + -1e11).replace(/[018]/g, c =>
@@ -19,10 +20,10 @@ function init() {
 }
 
 function get_image() {
-        var canvas = document.getElementById("image");
-        var ctx = canvas.getContext('2d');
-  ctx.clearRect(0, 0, canvas.width, canvas.height);
-  current_image = null;
+    var canvas = document.getElementById("image");
+    var ctx = canvas.getContext('2d');
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+    current_image = null;
     fetch('', {
         method: 'POST',
         headers: {
@@ -38,6 +39,7 @@ function get_image() {
 function set_image_on_doc(data) {
     var imagedata = new ImageData(new Uint8ClampedArray(data.data), data.width, data.height);
     current_image = { iid: data.iid };
+    start_time = Date.now();
     var aspect_ratio = data.width / data.height;
     var big_height = 300.0;
     var big_width = big_height * aspect_ratio;
@@ -73,21 +75,32 @@ function set_classes_on_doc(data) {
 }
 
 function store_result(cid) {
-  if (current_image) {
-    fetch('', {
-        method: 'POST',
-        headers: {
-            'Accept': 'application/json',
-            'Content-Type': 'application/json'
-        },
-      body: JSON.stringify({ "command": "store_result", "cid": cid, "iid": current_image.iid, "sid": session_id, "tt": -1.0 })
-    });
-    get_image();
-    progress += 1;
-    update_progress_text();
-  }
+    if (current_image) {
+        var end_time = Date.now();
+        var time_diff = end_time - start_time;
+        fetch('', {
+            method: 'POST',
+            headers: {
+                'Accept': 'application/json',
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({ "command": "store_result", "cid": cid, "iid": current_image.iid, "sid": session_id, "tt": time_diff })
+        });
+        get_image();
+        progress += 1;
+        update_progress_text();
+    }
 }
 
 function update_progress_text() {
-    console.log(progress);
+    var e = document.getElementById("thanksetc");
+
+    if (progress < 5) {
+    }
+    else if (progress < 10) {
+        e.innerHTML = "You've annotated a fair few images, thanks!"
+    }
+    else {
+        e.innerHTML = "You've annotated " + progress + " images! Thanks! Your efforts are much appreciated " + String.fromCodePoint(0x2764)
+    }
 }
