75d0dd42f45f7d5fa8b0eef3162a428353ae2032
Christopher Boyle <chrismjboyle@gmail.com>
2023-02-10 20:13:01 +0000

    Fix colours in imported images (BGR -> RGB)

--- add_images_to_database.py
+++ add_images_to_database.py
@@ -8,6 +8,7 @@ from imclasregan_server.database import _Database
 
 def add_image(name: str):
     image = cv2.imread(name, cv2.IMREAD_COLOR)
+    image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
     assert len(image.shape) == 3
     assert image.shape[2] == 3
     height, width = image.shape[:2]
