9124e2dddf6ce4f162b93cd83da13c1fcef0667c
Christopher Boyle <chrismjboyle@gmail.com>
2023-02-10 20:42:24 +0000

    set server port and ip by env var

--- src/main.rs
+++ src/main.rs
@@ -5,6 +5,9 @@ mod command;
 mod database;
 mod reply;
 
+use std::env;
+use std::net::Ipv4Addr;
+
 use rocket::fs::FileServer;
 use rocket::response::content;
 use rocket::serde::json::Json;
@@ -37,7 +40,21 @@ async fn command_handler(cmd: Json<Command<'_>>) -> content::RawJson<String> {
 
 #[launch]
 fn rocket() -> _ {
-    rocket::build()
+    let ip: Ipv4Addr = match env::var("IMCLASREGAN_IP") {
+        Ok(ip) => ip.as_str().parse().unwrap(),
+        Err(_) => "127.0.0.1".parse().unwrap(),
+    };
+    let port: u16 = match env::var("IMCLASREGAN_PORT") {
+        Ok(port) => port.as_str().parse().unwrap(),
+        Err(_) => "8008".parse().unwrap(),
+    };
+    let config = rocket::Config {
+        port: port,
+        address: ip.into(),
+        ..rocket::Config::debug_default()
+    };
+
+    rocket::custom(&config)
         .mount("/", FileServer::from("site/"))
         .mount("/", routes![command_handler])
 }
